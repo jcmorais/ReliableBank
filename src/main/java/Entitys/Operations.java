b1628560bcbfcae7effc92a0cb07bc772825a322
Carlos Morais <jcmorais93@gmail.com>
2016-05-15 00:20:11 +0100

    resize of operations

--- src/main/java/Entitys/Operations.java
+++ src/main/java/Entitys/Operations.java
@@ -12,6 +12,11 @@ public class Operations {
      * Onde são armezenadas todas as operações que vão ser feitas no Server
      * Apenas são guardadas operações de escrita (que provocam uma alteração ao estado)
      */
+
+    //só pq a memória não é infinita...
+    private static final int MAX_OP = 50000; //max de operations guardadas
+    private static final int CLEAN = 5000; //operações removidas, quando é atingido o MAX_OP
+
     private List<MessageInterface> operations;
     private Set<String> ids;
 
@@ -24,6 +29,9 @@ public class Operations {
     public void addMessage(MessageInterface msg){
         this.operations.add(msg);
         this.ids.add(msg.getMessageId());
+
+        if(this.operations.size() == MAX_OP)
+            this.operations = this.operations.subList(CLEAN, MAX_OP);
     }
 
     public List<MessageInterface> getListSinceId(String id){
